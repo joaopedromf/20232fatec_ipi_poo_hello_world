46c8356cd7635d69cbd053b7eaac5cc31c6b9a98
João Pedro Martins Forte <joao.forte@fatec.sp.gov.br>
2023-09-17 16:40:48 -0300

    ap01(java): hello world
    - Usando JOptionPane

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
+import javax.swing.JOptionPane;
 public class HelloWorld{
     public static void main(String[] args){
-        System.out.println("Hello, World");  
+        //System.out.println("Hello, World");
+        JOptionPane.showMessageDialog(null, "Hello, World");
     }
 }
